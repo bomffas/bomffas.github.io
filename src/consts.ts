5426056f4864b8b6105a0e5e5887ea7c9d3dffbd
Bomffas <oi@joaobomfim.com.br>
2025-07-26 22:47:14 -0300

    chore: title value

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
+export const SITE_TITLE = "Joao Bomfim";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const EMAIL_EN = "hi@joaobomfim.com";
 export const EMAIL_BR = "oi@joaobomfim.com.br";
